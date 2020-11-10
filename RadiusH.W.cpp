17ae43abc0f9e30dccbc812b1f936aac83070b5e
Eng.Alh <74233304+AlhTech@users.noreply.github.com>
2020-11-10 10:39:31 +0300

    Add files via upload

---
+++ RadiusH.W.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int radius, area;
+cout<<"Please insert value for radius length";
+cin>>radius;	
+cout<<"Area of given share is = "<<3.14159*(radius*radius); //3.14=p 
+
+return 0;
+}
